d46c9bfefe4f3a97287b88a923404f3bec999818
Blueasteroid <huangjiaqi@msn.com>
2015-12-01 15:58:08 +0000

    Update math24_solver.py

--- math24_solver.py
+++ math24_solver.py
@@ -6,25 +6,23 @@ import itertools
 import __future__
 
 print "\nSearching for answers ... \n"
-numlist = list(set(map("".join,itertools.permutations('44XX')))) 
+numlist = list(set(map("".join,itertools.permutations('789X')))) 
 #print numlist
 oplist = list(map("".join,itertools.product('+-*/', repeat=3)))
 #print oplist
 
 for i in numlist:
 	for j in oplist:
-		for k in range(6):
+		for k in range(5):
 			if k == 0: # '123':
 				exp = ( '((('+i[0] + j[0] + i[1]+')' + j[1] +i[2]+')' + j[2] +i[3]+')' ).replace('X','10')
-			elif k == 1: #'132':
+			elif k == 1: #'132' or k =='312':
 				exp = ( '(('+ i[0] + j[0] + i[1]+')' + j[1] +'('+i[2] + j[2] +i[3]+'))' ).replace('X','10')
 			elif k == 2: #'213':
 				exp = ( '(('+i[0] + j[0] + '('+ i[1] + j[1] +i[2]+'))' + j[2] +i[3]+')' ).replace('X','10')
 			elif k == 3: #'231':
 				exp = ( '('+ i[0] + j[0] + '(('+ i[1] + j[1] +i[2]+')' + j[2] +i[3]+'))' ).replace('X','10')
-			elif k == 4: #'312':
-				exp = ( '(('+ i[0] + j[0] + i[1]+')' + j[1] +'('+i[2] + j[2] +i[3]+'))' ).replace('X','10')
-			elif k == 5: #'321':
+			elif k == 4: #'321':
 				exp = ( '('+ i[0] + j[0] +'('+ i[1] + j[1] +'('+i[2] + j[2] +i[3]+')))' ).replace('X','10')	
 
 			try:
