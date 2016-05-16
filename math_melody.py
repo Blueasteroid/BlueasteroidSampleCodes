f3dfb1ad5e75ce82466387884360ef7f08f3564d
Blueasteroid <huangjiaqi@msn.com>
2016-05-16 21:25:37 +0100

    Update math_melody.py

--- math_melody.py
+++ math_melody.py
@@ -10,9 +10,13 @@ if len(sys.argv)>1:
 	musicnum=float(sys.argv[1])
 seq=str(Decimal(musicnum).sqrt()).replace('.','')	# sqrt melody
 '''
-
-seq=str(Decimal(1).exp()).replace('.','')	# e melody
-
+#seq=str(Decimal(1).exp()).replace('.','')	# e melody
+seq=str(Decimal( sum(1/Decimal(16)**k * 
+          (Decimal(4)/(8*k+1) - 
+           Decimal(2)/(8*k+4) - 
+           Decimal(1)/(8*k+5) -
+           Decimal(1)/(8*k+6)) for k in range(1000)))).replace('.','')	# pi (BBP formula) melody
+           
 print 'Now play...'
 note_list=[0,2,4,5,7,9,11,12 ,14,16]	# major tunes of equal temperament
 for num in seq: 
